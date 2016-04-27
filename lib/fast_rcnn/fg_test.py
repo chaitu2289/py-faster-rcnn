40c1a36c2c82fc486e5d37f2b152fb6312b4db16
Chaitanya <kchakka@cs.stonybrook.edu>
2016-04-27 14:43:22 -0400

    Code added to retrieve similar looking feature vectors

--- lib/fast_rcnn/fg_test.py
+++ lib/fast_rcnn/fg_test.py
@@ -122,12 +122,16 @@ def im_detect(net, im, boxes=None, feature_generation=True):
             background as object category 0)
         boxes (ndarray): R x (4*K) array of predicted bounding boxes
     """
+    boxes = np.append(boxes, np.array([[133, 169, 262, 316]]), axis=0)
     blobs, im_scales = _get_blobs(im, boxes)
+    import pdb
+    pdb.set_trace()
     ###
     #  Code added by chaitu
     ###
     if feature_generation:
-    	blobs['rois'] = np.array([np.append(0,box) for box in boxes])
+    	blobs['rois_'] = np.array([np.append(0,box) for box in boxes])
+	np.append(blobs['rois_'], np.array([0,133,169,262,316]))
     ###
     #  Code above added by chaitu
     ###
@@ -157,7 +161,7 @@ def im_detect(net, im, boxes=None, feature_generation=True):
         net.blobs['im_info'].reshape(*(blobs['im_info'].shape))
 	###code by chaitu
 	if feature_generation:
-		net.blobs['rois'].reshape(*(blobs['rois'].shape))
+		net.blobs['rois_'].reshape(*(blobs['rois_'].shape))
 	###code above by chaitu
     else:
         net.blobs['rois'].reshape(*(blobs['rois'].shape))
@@ -167,12 +171,14 @@ def im_detect(net, im, boxes=None, feature_generation=True):
     if cfg.TEST.HAS_RPN:
         forward_kwargs['im_info'] = blobs['im_info'].astype(np.float32, copy=False)
 	if feature_generation:
-		forward_kwargs['rois'] = blobs['rois'].astype(np.float32, copy=False)
+		forward_kwargs['rois_'] = blobs['rois_'].astype(np.float32, copy=False)
     else:
         forward_kwargs['rois'] = blobs['rois'].astype(np.float32, copy=False)
     #pdb.set_trace()
     blobs_out = net.forward(**forward_kwargs)
-    return net.blobs['fc7'].data
+    import pdb
+    pdb.set_trace()
+    return net.blobs['fc7'].data, net.blobs['cls_prob'].data
     #pdb.set_trace()
     #if feature_generation:
     #	feature_vector = net.blobs['fc7'].data
@@ -245,7 +251,7 @@ def apply_nms(all_boxes, thresh):
             nms_boxes[cls_ind][im_ind] = dets[keep, :].copy()
     return nms_boxes
 
-def test_net(net, imdb, image_id="009965"):
+def test_net(net, imdb, image_id="003202"):
     """Test a Fast R-CNN network on an image database."""
     num_images = len(imdb.image_index)
     # heuristic: keep an average of 40 detections per class per images prior
@@ -284,21 +290,28 @@ def test_net(net, imdb, image_id="009965"):
     ##
     image_index_chaitu = image_id
     box_proposals = _load_pascal_annotation(image_index_chaitu) 
-    box_proposals['boxes'] = np.array([box_proposals['boxes'][0]])
-    box_proposals['gt_classes'] = np.array([box_proposals['gt_classes'][0]])
+    #box_proposals['boxes'] = np.array([box_proposals['boxes'][0]])
+    #box_proposals['gt_classes'] = np.array([box_proposals['gt_classes'][0]])
+    box_proposals['boxes'] = box_proposals['boxes']
+    box_proposals['gt_classes'] = box_proposals['gt_classes']
     gt_new_image = box_proposals['gt_classes'][0]
     ##
     # code above added by chaitu
     ##
 
     im = cv2.imread('/var/services/homes/kchakka/py-faster-rcnn/data/VOCdevkit2007/VOC2007/JPEGImages/'+image_index_chaitu+'.jpg')
-    feature_vector_ = im_detect(net, im, box_proposals['boxes'])
+    import pdb
+    pdb.set_trace()
+    feature_vector_, clas_prob = im_detect(net, im, box_proposals['boxes'])
+    import pdb
+    pdb.set_trace()
     feature_vector_ = feature_vector_.squeeze()
     feature_vector_ = feature_vector_/np.linalg.norm(feature_vector_)
     max_fv_ = 0
     output = []
     dot_prod_values = []
-    #num_images = 200
+    #num_images = 10
+    m = {}
     for i in xrange(num_images):
 	print i
         # filter out any ground truth boxes
@@ -310,58 +323,70 @@ def test_net(net, imdb, image_id="009965"):
 	# code below added by chaitu
 	##
 	image_index_chaitu = imdb.image_path_at(i).split('/')[-1].split('.')[0] 
+	m[i] = {}
 	box_proposals = _load_pascal_annotation(image_index_chaitu) 
 	##
 	# code above added by chaitu
 	##
         im = cv2.imread(imdb.image_path_at(i))
         _t['im_detect'].tic()
-        fv_ = im_detect(net, im, box_proposals['boxes'])
+        fv_, class_prob = im_detect(net, im, box_proposals['boxes'])
+        import pdb
+	pdb.set_trace()
  	counter = 0
 	similar_label = []
 	non_similar_label = []
 	import sys
 	max_int = sys.maxint
-        for f_ in fv_:
+	for fv_i in range(len(fv_)):
+        #for f_ in fv_:
+		f_ = fv_[fv_i]
+		box = box_proposals['boxes'][fv_i]
 		f_ = f_.squeeze()
 		f_ = f_/np.linalg.norm(f_)
 		dot_prod = np.dot(feature_vector_, f_)
 		l2_distance = np.linalg.norm(feature_vector_ - f_)
-		similar_image_index = tuple((l2_distance, i, box_proposals['gt_classes'][counter]))
-		if len(output) < 20: 
-			output.append(similar_image_index)
-			output = sorted(output, reverse=True)
-		else:
-			if output[0][0] > l2_distance:
-				output.pop(0)
-				output.append(similar_image_index)
-				output = sorted(output, reverse=True)
-		counter += 1
+		similar_image_index = [dot_prod, i, fv_i,box_proposals['gt_classes'][counter]]
+		m[i][fv_i] = [box[0], box[1], box[2], box[3]]
+		output.append(similar_image_index)
+     
         _t['im_detect'].toc()
+    output = sorted(output, reverse=True)
+    #output = output[:20]
     train_txt = "/var/services/homes/kchakka/py-faster-rcnn/VOCdevkit/VOC2007/ImageSets/Main/train.txt"
     f = open(train_txt,'r')
     train_images = []
     similar_image_indices = []
     for line in f.readlines():
 	train_images.append(line.strip())
+    box_info = []
     for boxes in output:
 	similar_image_indices.append(train_images[boxes[1]])
-    print output
-    print similar_image_indices
+        box_info.append((train_images[boxes[1]], m[boxes[1]][boxes[2]]))
+	boxes.append(int(train_images[boxes[1]]))
+    print output[:20]
+    print "Similar Image Indices : " , similar_image_indices[:20]
+
+    import pdb
+    pdb.set_trace()
+    print "Images with box information : " , box_info
     for boxes in output:
-	if boxes[2] == gt_new_image:
-		similar_label.append(boxes)
+	if boxes[3] == gt_new_image:
+		if len(similar_label) <= 10:
+			similar_label.append(boxes)
 	else:
-		non_similar_label.append(boxes)
-    similar_label_size = len(similar_label)
-    non_similar_label = non_similar_label[:similar_label_size]
-	
+		if len(non_similar_label) <= 10:
+			non_similar_label.append(boxes)
+	if len(similar_label) > 10 and len(non_similar_label) > 10:
+		break
+    pdb.set_trace()	
     fine_tune_data = similar_label + non_similar_label
+    print fine_tune_data
     f = open('finetune.txt', 'w')
     for label in fine_tune_data:
-        id_length = len(str(label[1]))
+        id_length = len(str(label[4]))
 	zeros = "0"*(6-id_length)
-	f.write(zeros + str(label[1]) + "\n")
+	f.write(zeros + str(label[4]) + "\n")
     f.close() 
      
     
